007a3218dbe55d9adc66b757860cb7f1531bfe45
Rupert Tombs <rt500@cam.ac.uk>
2020-08-02 19:39:42 +0100

    Expand description. Add support for more integer types.

--- sar.c
+++ sar.c
@@ -1,109 +1,143 @@
 #include <limits.h>
 #include <stdint.h>
-#include <stdio.h>
 
-/* Portable branchless arithmetic right shift (sar) in c.
+/* Portable C arithmetic right shift (sar)
  *
  * The C specification leaves the right shift m >> n 
  * implementation-defined if m is a negative integer.
- *     (ISO/IEC 9899:TC2 6.5.7, p84-85)
+ *     (ISO/IEC 9899:TC2 6.5.7:5, p84-85)
  *
- * An arithmetic right shift inserts 1s in the high bits.
- * A logical right shifts inserts 0s in the high bits.
+ * The logical right shift inserts 0s in the high bits.
  * 
- * These tools ensure that negative integers have 1s inserted,
- * independent of which shift is given by the implementation.
+ * The arithmetic right shift inserts 1s in the high bits if the target
+ * m is negative, but inserts 0s otherwise.
+ * 
+ * This preserves its sign, and makes 
+ *      m >> n  equal to  m/2**n ,
+ * rounded towards -infinity.
+ * 
+ * The sar* functions below perform arithmetic right shifts, independent
+ * of which shift is given by the implementation.
  *
- * Code generated is the single shift instruction if the
- * implementation provides it, and always branchless.
+ * They compile to the single shift instruction if the implementation 
+ * provides it, and are always branchless and small.
  *
  * Context:
- * limits.h 
- *     CHAR_BIT
- *     // for long long int if available)
- *     LLONG_MAX 
  *
- * stdint.h
- *     // for fixed-width types if available)
- *     INT8_MAX INT16_MAX INT32_MAX INT64_MAX 
- *     int8_t int16_t int32_t int64_t
- *     UINT8_MAX UINT16_MAX UINT32_MAX UINT64_MAX 
- *     uint8_t uint16_t uint32_t uint64_t
+ * limits.h
+ *     CHAR_BIT LLONG_MAX
  * 
- * MIT license Copyright (c) 2020 Rupert Tombs
- *
- * TODO _Generic
+ * stdint.h 
+ *     integer types and macros implied by their presence
+ *  
+ * MIT LICENSE Copyright (c) 2020 Rupert Tombs
  */
 
 signed char sarc(signed char m, signed char n);
-short sars(short m, short n);
+short int sars(short int m, short int n);
 int sari(int m, int n);
 long int sarl(long int m, long int n);
 
-#if defined(LLONG_MAX) && defined(ULLONG_MAX)
+#ifdef LLONG_MAX
 long long int sarll(long long int m, long long int n);
 #endif
 
-#if defined(INT8_MAX) && defined(UINT8_MAX)
+/* exact-width integers optionally provided by stdint.h */
+#ifdef INT8_MAX
 int8_t sar8(int8_t m, int8_t n);
 #endif
 
-#if defined(INT16_MAX) && defined(UINT16_MAX)
+#ifdef INT16_MAX
 int16_t sar16(int16_t m, int16_t n);
 #endif
 
-#if defined(INT32_MAX) && defined(UINT32_MAX)
+#ifdef INT32_MAX
 int32_t sar32(int32_t m, int32_t n);
 #endif
 
-#if defined(INT64_MAX) && defined(UINT64_MAX)
+#ifdef INT64_MAX
 int64_t sar64(int64_t m, int64_t n);
+#endif
+
+#ifdef INTPTR_MAX
+intptr_t sarptr(intptr_t m, intptr_t n);
+#endif
+
+/* integers necessarily provided by stdint.h if included */
+#ifdef INT_LEAST8_MIN
+intmax_t sarmax(intmax_t m, intmax_t n);
+int_least8_t sarleast8(int_least8_t m, int_least8_t n);
+int_least16_t sarleast16(int_least16_t m, int_least16_t n);
+int_least32_t sarleast32(int_least32_t m, int_least32_t n);
+int_least64_t sarleast64(int_least64_t m, int_least64_t n);
+int_fast8_t sarfast8(int_fast8_t m, int_fast8_t n);
+int_fast16_t sarfast16(int_fast16_t m, int_fast16_t n);
+int_fast32_t sarfast32(int_fast32_t m, int_fast32_t n);
+int_fast64_t sarfast64(int_fast64_t m, int_fast64_t n);
 #endif
 
 /* Macro to define these declared functions.
  *
  * Arguments:
  *     label is the suffix to sar.
- *     type is the target signed integer type.
- *     utype is an unsigned equivalent of type.
+ *     type is the signed integer type.
+ *     utype is the unsigned cousin of type with the same size as type.
  * 
  * Explanation:
  *     logical flags if the implementation logically right shifts type.
  *     fill is all 1s if the high bits need to be filled.
  * 
- * we then left shift fill (which is defined since it is unsigned)
- * to get 1s in the high bits, then logical or (|) to insert them.
+ * we then left shift fill (which is defined since it is unsigned) to 
+ * get 1s in the high bits, then logical or to set them.
+ * The c standard re
  */
-#define SARDEFINE(label, type, utype) \
-    type \
-    sar##label(type m, type n) \
-    { \
-        const type logical = ((type)-1 >> 1) > 0; \
-        utype fill = -(logical && m < 0); \
-        return (m >> n) | (fill << (CHAR_BIT*sizeof(m) - n)); \
+#define SARDEFINE(label, type, utype)                                  \
+    type                                                               \
+    sar##label(type m, type n)                                         \
+    {                                                                  \
+        const type logical = ((type)-1 >> 1) > 0;                      \
+        utype fill;                                                    \
+        fill = -(logical && m < 0);                                    \
+        return (m >> n) | (fill << (CHAR_BIT*sizeof(m) - n));          \
     }
 
 SARDEFINE(c, signed char, unsigned char)
-SARDEFINE(s, short, unsigned short)
+SARDEFINE(s, short int, unsigned short int)
 SARDEFINE(i, int, unsigned int)
 SARDEFINE(l, long int, unsigned long int)
 
-#if defined(LLONG_MAX) && defined(ULLONG_MAX)
+#ifdef LLONG_MAX
 SARDEFINE(ll, long long int, unsigned long long int)
 #endif
 
-#if defined(INT8_MAX) && defined(UINT8_MAX)
+#ifdef INT8_MAX
 SARDEFINE(8, int8_t, uint8_t)
 #endif
 
-#if defined(INT16_MAX) && defined(UINT16_MAX)
+#ifdef INT16_MAX
 SARDEFINE(16, int16_t, uint16_t)
 #endif
 
-#if defined(INT32_MAX) && defined(UINT32_MAX)
+#ifdef INT32_MAX
 SARDEFINE(32, int32_t, uint32_t)
 #endif
 
-#if defined(INT64_MAX) && defined(UINT64_MAX)
+#ifdef INT64_MAX
 SARDEFINE(64, int64_t, uint64_t)
+#endif
+
+#ifdef INTPTR_MAX
+SARDEFINE(ptr, intptr_t, uintptr_t)
+#endif
+
+#ifdef INT_LEAST8_MIN
+SARDEFINE(max, intmax_t, uintmax_t)
+SARDEFINE(least8, int_least8_t, int_least8_t)
+SARDEFINE(least16, int_least16_t, int_least16_t)
+SARDEFINE(least32, int_least32_t, int_least32_t)
+SARDEFINE(least64, int_least64_t, int_least64_t)
+SARDEFINE(fast8, int_fast8_t, int_fast8_t)
+SARDEFINE(fast16, int_fast16_t, int_fast16_t)
+SARDEFINE(fast32, int_fast32_t, int_fast32_t)
+SARDEFINE(fast64, int_fast64_t, int_fast64_t)
 #endif
